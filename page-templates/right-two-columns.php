b8ccef6ca5c144667d3f8acf7ab21666f66868f3
priscillamc <priscillamc@users.noreply.github.com>
2015-12-02 21:20:42 -0500

    Makes right two column page template flexible content width
    Behaves more like the ufclaspeople theme

--- page-templates/right-two-columns.php
+++ page-templates/right-two-columns.php
@@ -3,9 +3,14 @@
 Template Name: Two Columns (Right Sidebar, No Left Column)
 */
 
-get_header(); ?>
-
-	<div id="primary" class="content-area col-sm-9">
+get_header(); 
+?> 	
+	<?php 
+		// Set width of primary area, full width when no page_right widgets present
+		$primary_width = ( is_active_sidebar( 'page_right' ) )? ' col-sm-9':' col-sm-12';
+	?>
+ 
+	<div id="primary" class="content-area<?php echo $primary_width; ?>">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
