b32487142bc31ee6c51b9add4cc7830ceb9d082d
priscillamc <priscillamc@users.noreply.github.com>
2017-03-01 12:17:07 -0500

    Updates Google Analytics ID, fixes styles for page max-width, adds compass config file

--- header.php
+++ header.php
@@ -20,18 +20,19 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php if( !is_user_logged_in() ) { ?>
+<?php 
+if( !is_user_logged_in() ): ?>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-1145687-25', 'auto');
+  ga('create', 'UA-63756341-3', 'auto');
   ga('send', 'pageview');
 
 </script>
-<?php } ?>
+<?php endif; ?>
 <nav id="institutional-nav" role="navigation">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'ufclaspeople2' ); ?></a>
   <ul id="top" class="container">
